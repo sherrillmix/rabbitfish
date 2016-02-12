fda5aa4d6dee7869aefebb76491a08d8fa6ce1a8
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-02-12 14:39:30 -0500

    Add make index script

---
+++ makeIndex.bash
@@ -0,0 +1,3 @@
+
+#https://genome.ucsc.edu/cgi-bin/hgTables?hgsid=472902033_IrrF70nkVeCgZvwkOZA3A16xUUp4&clade=vertebrate&org=Zebrafish&db=danRer10&hgta_group=genes&hgta_track=ensGene&hgta_table=0&hgta_regionType=genome&position=chr1%3A58631729-58641660&hgta_outputType=primaryTable&hgta_outFileName=
+STAR/bin/Linux_x86_64_static/STAR   --runMode genomeGenerate   --runThreadN 24   --genomeDir index/ --sjdbGTFfile zebrafishgenome/Drerio_ensGene.GTF --genomeFastaFiles zebrafishgenome/danRer10.fa
