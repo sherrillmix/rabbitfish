2c702f39b8d78352b5ef686999d95f4a8d87263e
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-03-18 11:26:41 -0400

    Status messages

--- runTrinity.bash
+++ runTrinity.bash
@@ -3,15 +3,19 @@
 
 #pairs genome guided
 if [ ! -f "work/trinity/Trinity-GG.fasta" ];then
+	echo "Running genome guided"
 	~/installs/trinity/Trinity --seqType fq --left data/UTAH_CORE/101Paired_end/9386R/Fastq/9386X1_140606_D00294_0102_AC4JVKACXX_6_1.txt.gz --right data/UTAH_CORE/101Paired_end/9386R/Fastq/9386X1_140606_D00294_0102_AC4JVKACXX_6_2.txt.gz  --CPU 12 --max_memory 120G --output work/trinity --trimmomatic --genome_guided_bam work/9386X1_140606_D00294_0102_AC4JVKACXX_6.bam --genome_guided_max_intron 40000
 fi
 
 #pairs non genome guided
 if [ ! -f "work/trinity_noGenome/Trinity-GG.fasta" ];then
+	echo "Running nongenome guided paired"
 	~/installs/trinity/Trinity --seqType fq --left data/UTAH_CORE/101Paired_end/9386R/Fastq/9386X1_140606_D00294_0102_AC4JVKACXX_6_1.txt.gz --right data/UTAH_CORE/101Paired_end/9386R/Fastq/9386X1_140606_D00294_0102_AC4JVKACXX_6_2.txt.gz  --CPU 12 --max_memory 120G --output work/trinity_noGenome --trimmomatic
 fi
 
 #everything no pairs
 if [ ! -f "work/trinity_unpaired/Trinity-GG.fasta" ];then
+	echo "Running nongenome guided all data unpaired"
 	~/installs/trinity/Trinity --seqType fq --single data/UTAH_CORE/101Paired_end/9386R/Fastq/9386X1_140606_D00294_0102_AC4JVKACXX_6_[1].txt.gz data/UGUAM0001JB/RNAseq_RawData/Samples/*_sequence.txt.gz  --CPU 12 --max_memory 120G --output work/trinity_unpaired --trimmomatic
 fi
+
