02375308eebfba1f252ddd7fb032565cdfc716d6
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-03-29 08:16:29 -0400

    Fix typos

--- annotateTrinity.bash
+++ annotateTrinity.bash
@@ -4,7 +4,7 @@ set -e
 
 #following http://transdecoder.github.io/
 for dir in work/trinity_noGenome work/trinity_unpaired;do
-	if [ ! -f "work/$dir/transdecode/longest_orfs.pep" ];then
+	if [ ! -f "$dir/transdecode/longest_orfs.pep" ];then
 		#cd to avoid permission issue with Transdecoder forced directory path
 		cd $dir
 		echo "Transdecoding $dir"
@@ -16,7 +16,21 @@ done
 
 #following https://trinotate.github.io/#SequencesRequired
 for dir in work/trinity_noGenome work/trinity_unpaired;do
-	blastx -query $dir/Trinity.fasta -db ~/installs/trinotate/db/uniprot_sprot.pep -num_threads 12 -max_target_seqs 1 -outfmt 6 > $dir/blastx.outfmt6
-	blastp -query $dir/transdecoder.pep -db ~/installs/trinotate/db/uniprot_sprot.pep -num_threads 12 -max_target_seqs 1 -outfmt 6 > $dir/blastp.outfmt6
-	hmmscan --cpu 12 --domtblout $dir/TrinotatePFAM.out ~/installs/trinotate/db/Pfam-A.hmm $dir/transdecode/longest_orfs.pep > $dir/pfam.log
+	outFile=$dir/blastx.outfmt6
+	if [ ! -f "$outFile" ];then
+		echo "blastx $dir"
+		blastx -query $dir/Trinity.fasta -db ~/installs/trinotate/db/uniprot_sprot.pep -num_threads 12 -max_target_seqs 1 -outfmt 6 > $outFile
+	fi
+	outFile=$dir/blastp.outfmt6
+	if [ ! -f "$outFile" ];then
+		echo "blastp $dir"
+		blastp -query $dir/transdecode/longest_orfs.pep -db ~/installs/trinotate/db/uniprot_sprot.pep -num_threads 12 -max_target_seqs 1 -outfmt 6 > $outFile
+	fi
+	outFile=$dir/pfam.log
+	if [ ! -f "$outFile" ];then
+		echo "hmmscan $dir"
+		hmmscan --cpu 12 --domtblout $dir/TrinotatePFAM.out ~/installs/trinotate/db/Pfam-A.hmm $dir/transdecode/longest_orfs.pep > $outFile
+	fi
 done
+
+echo "All done"
