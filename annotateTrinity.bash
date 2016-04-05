4440e19b2552eb4ca3970793310bf34a1d4dbb32
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-04-05 14:02:52 -0400

    Got trinotate working

--- annotateTrinity.bash
+++ annotateTrinity.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+trinonateDir=~/installs/trinotate
 
 #following http://transdecoder.github.io/
 for dir in work/trinity_noGenome work/trinity_unpaired;do
@@ -32,5 +33,40 @@ for dir in work/trinity_noGenome work/trinity_unpaired;do
 		hmmscan --cpu 12 --domtblout $dir/TrinotatePFAM.out ~/installs/trinotate/db/Pfam-A.hmm $dir/transdecode/longest_orfs.pep > $outFile
 	fi
 done
+
+
+if [ ! -f "work/Trinotate.sqlite" ];then
+	wget "https://data.broadinstitute.org/Trinity/Trinotate_v3_RESOURCES/Trinotate_v3.sqlite.gz" -O work/Trinotate.sqlite.gz
+	gunzip -f work/Trinotate.sqlite.gz
+fi
+
+
+for dir in work/trinity_noGenome work/trinity_unpaired;do
+	echo "Loading $dir"
+	echo "Copy premade sqlite db"
+	cp work/Trinotate.sqlite $dir
+	#Trinotate writes to temp file in current directory so cd to work dir
+	cd $dir
+	echo "Gene map"
+	~/installs/trinity/util/support_scripts/get_Trinity_gene_to_trans_map.pl Trinity.fasta >  Trinity.fasta.gene_trans_map
+	echo "Init"
+	$trinonateDir/Trinotate Trinotate.sqlite init --gene_trans_map Trinity.fasta.gene_trans_map --transcript_fasta Trinity.fasta --transdecoder_pep transdecode/longest_orfs.pep
+	echo "Load protein hits"
+	$trinonateDir/Trinotate Trinotate.sqlite LOAD_swissprot_blastp blastp.outfmt6
+	echo "Load transcript hits"
+	$trinonateDir/Trinotate Trinotate.sqlite LOAD_swissprot_blastx blastx.outfmt6
+	echo "Load Pfam"
+	$trinonateDir/Trinotate Trinotate.sqlite LOAD_pfam TrinotatePFAM.out
+	echo "make report"
+	$trinonateDir/Trinotate Trinotate.sqlite report  > trinotate_annotation_report.xls
+	cd -
+done
+
+
+
+
+
+
+
 
 echo "All done"
